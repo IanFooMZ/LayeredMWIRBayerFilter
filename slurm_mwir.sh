567f25ff860bcd84bf5ff6737bf7895c1b325722
Ian Foo <ianfoomz@gmail.com>
2022-01-18 20:34:53 -0800

    Revert "Updated optimization code for higher angles (v10)"
    This reverts commit 04e681febe73cc21f59a6b32d2ef913cf83444a9.

--- slurm_mwir.sh
+++ slurm_mwir.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#SBATCH --time=167:00:00
-#SBATCH --nodes=10
+#SBATCH --time=144:00:00
+#SBATCH --nodes=5
 #SBATCH --ntasks-per-node=8
 #SBATCH --qos=normal
-#SBATCH --mem-per-cpu=8G
+#SBATCH --mem-per-cpu=30G
 #SBATCH --mail-user=ifoo@caltech.edu
 #SBATCH --mail-type=END
 
 source activate fdtd
 
-xvfb-run --server-args="-screen 0 1280x1024x24" python LayeredMWIRBridgesBayerFilterOptimization.py 5 > stdout_mwir_g40.log 2> stderr_mwir_g40.log
+xvfb-run --server-args="-screen 0 1280x1024x24" python LayeredMWIRBridgesBayerFilterOptimization.py 5 > stdout_mwir_g5.log 2> stderr_mwir_g5.log
 
 exit $?
